ffb3bd53229b70a0bb32f8a60ca4c9389bb10038
Shaik Sajid Ahamed <sajidahamedshaik@gmail.com>
2025-08-08 00:21:49 +0530

    added form validations to Register,Login pages

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -8,41 +8,107 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const [errorObj, setErrorObj] = useState<{ [key : string] : string}>({})
+
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setErrorObj((prev) => {
+      const { submit, ...rest } = prev;
+      return rest;
+    });
+    if(e.target.name === 'email'){
+      setEmail(e.target.value);
+      handleEmail(e.target.value);
+    }else {
+      setPassword(e.target.value);
+      handlePassword(e.target.value);
+    }
+  }
+
+  const handleEmail = (value : string) => {
+    let error = "";
+    if(!value.trim()){
+      error = "Email is required";
+      setErrorObj((prev) => ({ ...prev, email: error }));
+    } else if (!/^(?![-.])(?!.*[-.]@)(?!.*[.-]{2})[\w.-]+\w@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
+      error = "Invalid email format";
+      setErrorObj((prev) => ({ ...prev, email: error }));
+    } else {
+      setErrorObj((prev) => {
+        const { email, ...rest } = prev;
+        return rest;
+      });
+    }
+  }
+
+  const handlePassword = (value: string) => {
+    let error = "";
+    if (!value) {
+      error = "Password is required";
+      setErrorObj((prev) => ({ ...prev, password: error }));
+    } else { 
+      setErrorObj((prev) => {
+        const { password, ...rest } = prev;
+        return rest;
+      });
+    }
+  }
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+    handleEmail(email);
+    handlePassword(password);
+    if( Object.keys(errorObj).length > 0 ) return;
+
     try {
       const res = await axios.post(`${API_URL}/api/auth/login`, {
         email,
         password,
       });
-
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       navigate('/');
     } catch (err) {
-      alert('Login failed');
+      setErrorObj((prev) => ({ ...prev, submit : 'Invalid Credentials' }));
     }
   };
+
+   const isFormValid = 
+    email.trim() !== "" &&
+    password !== "" &&
+    Object.keys(errorObj).length === 0;
 
   return (
     <div className="auth-container fade-in">
       <form className="auth-card slide-up" onSubmit={handleLogin}>
         <h2>Login</h2>
-        <input
-          className="auth-input"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input
-          className="auth-input"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button className="auth-btn" type="submit">Login</button>
+          <div className='input-error-wrapper'>
+            <input
+              className="auth-input"
+              name='email'
+              value={email}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            {errorObj.email && <p className="error-text">{errorObj.email}</p>}
+          </div>
+        <div className="input-error-wrapper">
+          <input
+            className="auth-input"
+            name="password"
+            type="password"
+            value={password}
+            onChange={handleChange}
+            placeholder="Password"
+          />
+          {errorObj.password && (
+            <p className="error-text">{errorObj.password}</p>
+          )}
+        </div>
+
+        <button className="auth-btn" type="submit" disabled={!isFormValid}>Login</button>
+        {errorObj.submit && (
+            <p className="error-text" style={{textAlign:'center'}}>{errorObj.submit}</p>
+          )}
         <p>
           Don’t have an account? <Link to="/register">Register</Link>
         </p>
--- frontend/src/features/auth/Register.tsx
+++ frontend/src/features/auth/Register.tsx
@@ -1,26 +1,100 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-import { API_URL } from '../../lib/api';
-import '../../App.css';
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { API_URL } from "../../lib/api";
+import "../../App.css";
 
 const Register = () => {
-  const [form, setForm] = useState({ username: '', email: '', password: '' });
+  const [form, setForm] = useState({ username: "", email: "", password: "" });
   const navigate = useNavigate();
+  const [errorObj, setErrorObj] = useState<{ [key: string]: string }>({});
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+    validate(e);
   };
+
+  const validate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "username") {
+      handleName(value);
+    } else if (name === "email") {
+      handleEmail(value);
+    } else if (name === "password") {
+      handlePassword(value);
+    }
+  };
+
+  const handleName = (value: string) => {
+    let error = "";
+    if (!value.trim()) {
+      error = "Username is required";
+      setErrorObj((prev) => ({ ...prev, username: error }));
+    } else {
+      setErrorObj((prev) => {
+        const { username, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+
+  const handleEmail = (value: string) => {
+    let error = "";
+    if (!value.trim()) {
+      error = "Email is required";
+      setErrorObj((prev) => ({ ...prev, email: error }));
+    } else if (!/^(?![-.])(?!.*[-.]@)(?!.*[.-]{2})[\w.-]+\w@([\w-]+\.)+[\w-]{2,4}$/.test(value)) {
+      error = "Invalid email format";
+      setErrorObj((prev) => ({ ...prev, email: error }));
+    } else {
+      setErrorObj((prev) => {
+        const { email, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+
+  const handlePassword = (value: string) => {
+    let error = "";
+    if (!value) {
+      error = "Password is required";
+      setErrorObj((prev) => ({ ...prev, password: error }));
+    } else if (
+      // !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(value) //a-z, A-Z, 0-9, special character finally length=8+ char
+      !/^[a-zA-Z\d\W_]{8,}$/.test(value) //minimun 8 characters
+    ) {
+      error =
+        "Password must be at least 8 characters";
+      setErrorObj((prev) => ({ ...prev, password: error }));
+    } else {
+      setErrorObj((prev) => {
+        const { password, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+
+  const isFormValid =
+    form.username.trim() !== "" &&
+    form.email.trim() !== "" &&
+    form.password !== "" &&
+    Object.keys(errorObj).length === 0;
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (Object.keys(errorObj).length > 0) {
+      setErrorObj(errorObj);
+      return;
+    }
+
     try {
       const res = await axios.post(`${API_URL}/api/auth/register`, form);
-      localStorage.setItem('token', res.data.token);
-      localStorage.setItem('user', JSON.stringify(res.data.user));
-      navigate('/');
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      navigate("/");
     } catch (err) {
-      alert('Registration failed');
+      alert("Registration failed");
     }
   };
 
@@ -28,29 +102,49 @@ const Register = () => {
     <div className="auth-container fade-in">
       <form className="auth-card slide-up" onSubmit={handleRegister}>
         <h2>Register</h2>
-        <input
-          className="auth-input"
-          name="username"
-          placeholder="Username"
-          value={form.username}
-          onChange={handleChange}
-        />
-        <input
-          className="auth-input"
-          name="email"
-          placeholder="Email"
-          value={form.email}
-          onChange={handleChange}
-        />
-        <input
-          className="auth-input"
-          name="password"
-          placeholder="Password"
-          type="password"
-          value={form.password}
-          onChange={handleChange}
-        />
-        <button className="auth-btn" type="submit">Register</button>
+        <div className="input-error-wrapper">
+          <input
+            className={`auth-input ${errorObj.username ? "input-error" : ""}`}
+            name="username"
+            placeholder="Username"
+            value={form.username}
+            onChange={handleChange}
+          />
+          {errorObj.username && (
+            <p className="error-text">{errorObj.username}</p>
+          )}
+        </div>
+        <div className="input-error-wrapper">
+          <input
+            className={`auth-input ${errorObj.email ? "input-error" : ""}`}
+            name="email"
+            placeholder="Email"
+            value={form.email}
+            onChange={handleChange}
+          />
+          {errorObj.email && <p className="error-text">{errorObj.email}</p>}
+        </div>
+        <div className="input-error-wrapper">
+          <input
+            className={`auth-input ${errorObj.password ? "input-error" : ""}`}
+            name="password"
+            placeholder="Password"
+            type="password"
+            value={form.password}
+            onChange={handleChange}
+          />
+          {errorObj.password && (
+            <p className="error-text">{errorObj.password}</p>
+          )}
+        </div>
+        <div
+          className="button-tooltip-wrapper"
+          title={!isFormValid ? "Please fill out all fields correctly." : ""}
+        >
+          <button className="auth-btn" type="submit" disabled={!isFormValid}>
+            Register
+          </button>
+        </div>
         <p>
           Already have an account? <Link to="/login">Login</Link>
         </p>
